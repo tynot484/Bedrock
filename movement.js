087ff1077bf15f2c1d386fe2bb65543908fec2be
tynot484 <cr7messs30@gmail.com>
2025-12-16 12:08:16 +0100

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,9 +1,17 @@
+const { rand } = require('../utils/random')
+const { randomDelay } = require('../utils/delay')
+
 module.exports = async (client) => {
-  const actions = ['forward', 'back', 'left', 'right', 'jump']
-  const action = actions[Math.floor(Math.random() * actions.length)]
+  const x = rand(-1, 1)
+  const z = rand(-1, 1)
+  const jump = Math.random() < 0.25
 
   client.queue('player_auth_input', {
-    move_vector: { x: Math.random(), y: 0, z: Math.random() },
-    input_data: { [action]: true }
+    move_vector: { x, y: 0, z },
+    input_data: {
+      jump
+    }
   })
+
+  await randomDelay(800, 3000)
 }
